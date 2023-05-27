ddf1492da3ba6efd1166d2f1a323db432f6c9d02
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-05-27 21:55:43 +0530

    backend link added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const app_config = {
-    app_url:"http://localhost:5000"
+    // app_url:"http://localhost:5000"
+    app_url:"https://rehnuma-calculator-task-backend.onrender.com"
 }
 export default app_config;
